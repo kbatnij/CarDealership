48154dcbd18290562bf20b351ea621d1714f6e23
Khaldon Batnij <Batnij.k@gmail.com>
2018-06-14 12:39:38 -0700

    Display all cars

--- ClientApp/app/components/cars/cars.component.ts
+++ ClientApp/app/components/cars/cars.component.ts
@@ -1,15 +1,26 @@
+import { CarService } from './../../services/car.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.css']
+  styleUrls: ['./cars.component.css'],
+  providers: [CarService]
 })
 export class CarsComponent implements OnInit {
-
-  constructor() { }
+  cars: any = {};
+  columns: any = [
+    { title: 'Make'},
+    { title: 'Year'},
+    { title: 'Color'},
+    { title: 'Price'},
+    { }
+  ];
+  constructor(private carService: CarService) { }
 
   ngOnInit() {
+    this.carService.getCars()
+      .subscribe(result => this.cars = result);
   }
 
 }
--- ClientApp/app/services/car.service.ts
+++ ClientApp/app/services/car.service.ts
@@ -1,10 +1,15 @@
-import { Http } from '@angular/http';
-import { Injectable } from '@angular/core';
+import { Http } from "@angular/http";
+import { Injectable } from "@angular/core";
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class CarService {
-    private readonly carsEndpoint = '/api/cars';
+  private readonly carsEndpoint = "/api/cars";
 
-constructor(private http: Http) { }
-
+  constructor(private http: Http) {}
+  getCars() {
+    return this.http
+      .get(this.carsEndpoint)
+      .map(res => res.json());
+  }
 }
