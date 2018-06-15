1d4a3870b72d3c473d65f2e27aaf9017932b04ab
Khaldon Batnij <Batnij.k@gmail.com>
2018-06-15 02:49:17 -0700

    Cleanup

--- Controllers/CarsController.cs
+++ Controllers/CarsController.cs
@@ -4,7 +4,6 @@ using CarDealership.Controllers.Resources;
 using CarDealership.Models;
 using CarDealership.Presistence;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace CarDealership.Controllers
 {
